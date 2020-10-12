39367998f7bf64fb44c1f4f17f2818a457ddab09
Adriane Hanson <ahansonuga@gmail.com>
2020-10-12 16:11:43 -0400

    Reformatting, reports.py
    Used PyCharm to automatically reform to better fit Python guidelines.

--- scripts/reports.py
+++ scripts/reports.py
@@ -1,6 +1,5 @@
-"""
-Calculate various subtotal reports of the collection, aip, and file counts for format types, format standard names, and groups. The reports are saved as csv files.
-"""
+"""Calculate various subtotal reports of the collection, aip, and file counts for format types, format standard
+names, and groups. The reports are saved as csv files. """
 
 # TODO: Sorts A-Z and then a-z. Can we sort case insensitive, or is sorting unnecessary since can sort in Excel?
 # TODO: could save each report as a tab in an Excel file instead of individual CSVs.
@@ -10,23 +9,23 @@ Calculate various subtotal reports of the collection, aip, and file counts for f
 
 import pandas as pd
 
-#formats is the data to analyze
-#results is the folder to save reports to
+# formats is the data to analyze
+# results is the folder to save reports to
 formats = 'H:/ARCHive-formats/archive_formats_2020-03.csv'
 results = 'H:/ARCHive-formats/2020-02-25-reports'
 
-#Read the data from the csv.
+# Read the data from the csv.
 df = pd.read_csv(formats)
 
-#Format type.
+# Format type.
 type_totals = df.groupby('Format_Type').sum()
 type_totals.to_csv(f'{results}/type_totals.csv')
 
-#Format standard name.
+# Format standard name.
 name_totals = df.groupby('Format_Standard_Name').sum()
 name_totals.to_csv(f'{results}/name_totals.csv')
 
-#Group.
+# Group.
 group_totals = df.groupby('Group').sum()
 group_totals.to_csv(f'{results}/group_totals.csv')
 
@@ -41,5 +40,3 @@ type_name.to_csv(f'{results}/type_name.csv')
 # Format standardized name subdivided by group.
 name_group = df.groupby(['Format_Standard_Name', 'Group']).sum()
 name_group.to_csv(f'{results}/name_group.csv')
-
-
