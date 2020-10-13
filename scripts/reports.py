fd8632305eef54f8ad348e94c9c1f0d95b8556d4
Adriane Hanson <ahansonuga@gmail.com>
2020-10-13 10:00:11 -0400

    Save to Excel instead, reports.py
    Instead of saving a seprate csv for each subtotal, save each as a tab in Excel. Easier to keep them grouped together.

--- scripts/reports.py
+++ scripts/reports.py
@@ -18,43 +18,30 @@ import sys
 formats = sys.argv[1]
 results = sys.argv[2]
 
-# Gets the current date, formatted YYYY-MM, to use in naming the reports.
+# Gets the current date, formatted YYYY-MM, to use in naming the results spreadsheet.
 today = datetime.datetime.now().strftime("%Y-%m")
 
 # Read the data from the csv.
 df = pd.read_csv(formats)
 
-# For each report, uses pandas to calculate the collection count, AIP count, and file count for each instance of the
-# type of information in the report, for example each format category. Then saves the result to a CSV in the results
-# folder. A separate CSV is made for each report.
+# For each field or field combination to analyze, uses pandas to calculate the collection count, AIP count, and file
+# count for each instance of the field. Fields are:
+#       Format type: category of the format, for example audio, image, or text.
+#       Format standardized name: a simplified version of the name, for example removing version information.
+#       Group: ARCHive group name, which is the department or departments responsible for the content.
 
-# Format type, for example audio, image, and text.
 type = df.groupby('Format_Type').sum()
-type.to_csv(f'{results}/type_{today}.csv')
-
-# Format standardized name, which is a simplified version of the name to group related formats (e.g. different
-# versions or subcategories of the same format) together.
 name = df.groupby('Format_Standard_Name').sum()
-name.to_csv(f'{results}/name_{today}.csv')
-
-# ARCHive group, which is the department or departments responsible for the content.
-# If a group is in ARCHive but does not have any AIPs yet, it will not be included in this report.
 group = df.groupby('Group').sum()
-group.to_csv(f'{results}/group_{today}.csv')
-
-# Format type subdivided by group.
 type_group = df.groupby(['Format_Type', 'Group']).sum()
-type_group.to_csv(f'{results}/type_group_{today}.csv')
-
-# Format type subdivided by format standardized name.
 type_name = df.groupby(['Format_Type', 'Format_Standard_Name']).sum()
-type_name.to_csv(f'{results}/type_name_{today}.csv')
-
-# Format standardized name subdivided by group.
 name_group = df.groupby(['Format_Standard_Name', 'Group']).sum()
-name_group.to_csv(f'{results}/name_group_{today}.csv')
 
-# # Save each of the subtotals to a tab in the same Excel spreadsheet.
-# with pd.ExcelWriter(f'{results}/format_analysis.xlsx') as writer:
-#     type_totals.to_excel(writer, sheet_name='format type')
-#     name_totals.to_excel(writer, sheet_name='format name')
+# Saves the counts for each field or field combination to a separate tab in the same Excel spreadsheet.
+with pd.ExcelWriter(f'{results}/format_analysis_{today}.xlsx') as writer:
+    type.to_excel(writer, sheet_name='format type')
+    name.to_excel(writer, sheet_name='format name')
+    group.to_excel(writer, sheet_name='group')
+    type_group.to_excel(writer, sheet_name='format type then group')
+    type_name.to_excel(writer, sheet_name='format type then name')
+    name_group.to_excel(writer, sheet_name='format name then group')
