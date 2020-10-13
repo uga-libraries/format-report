2cf9fb373e167d76fe161b6d414d61c76a3a1c13
Adriane Hanson <ahansonuga@gmail.com>
2020-10-13 09:29:12 -0400

    Include date in filename, reports.py
    Year and month included in each report filename to have a permanent record of when the data is from.
    
    Also removed _totals from filenames so all naming is more consistent.

--- scripts/reports.py
+++ scripts/reports.py
@@ -7,6 +7,7 @@ names, and groups. The reports are saved as csv files. """
 # TODO: could generate a text document with summary information.
 # TODO: may have better analysis of "frequent" if include size as well. Shawn would need to add to the format report.
 
+import datetime
 import pandas as pd
 import sys
 
@@ -17,35 +18,38 @@ import sys
 formats = sys.argv[1]
 results = sys.argv[2]
 
+# Gets the current date, formatted YYYY-MM, to use in naming the reports.
+today = datetime.datetime.now().strftime("%Y-%m")
+
 # Read the data from the csv.
 df = pd.read_csv(formats)
 
-# For each report, Calculates the collection count, AIP count, and file count for each instance of the
+# For each report, uses pandas to calculate the collection count, AIP count, and file count for each instance of the
 # type of information in the report, for example each format category. Then saves the result to a CSV in the results
 # folder. A separate CSV is made for each report.
 
 # Format type, for example audio, image, and text.
 type_totals = df.groupby('Format_Type').sum()
-type_totals.to_csv(f'{results}/type_totals.csv')
+type_totals.to_csv(f'{results}/type_{today}.csv')
 
 # Format standardized name, which is a simplified version of the name to group related formats (e.g. different
 # versions or subcategories of the same format) together.
 name_totals = df.groupby('Format_Standard_Name').sum()
-name_totals.to_csv(f'{results}/name_totals.csv')
+name_totals.to_csv(f'{results}/name_{today}.csv')
 
 # ARCHive group, which is the department or departments responsible for the content.
 # If a group is in ARCHive but does not have any AIPs yet, it will not be included in this report.
 group_totals = df.groupby('Group').sum()
-group_totals.to_csv(f'{results}/group_totals.csv')
+group_totals.to_csv(f'{results}/group_{today}.csv')
 
 # Format type subdivided by group.
 type_group = df.groupby(['Format_Type', 'Group']).sum()
-type_group.to_csv(f'{results}/type_group.csv')
+type_group.to_csv(f'{results}/type_group_{today}.csv')
 
 # Format type subdivided by format standardized name.
 type_name = df.groupby(['Format_Type', 'Format_Standard_Name']).sum()
-type_name.to_csv(f'{results}/type_name.csv')
+type_name.to_csv(f'{results}/type_name_{today}.csv')
 
 # Format standardized name subdivided by group.
 name_group = df.groupby(['Format_Standard_Name', 'Group']).sum()
-name_group.to_csv(f'{results}/name_group.csv')
+name_group.to_csv(f'{results}/name_group_{today}.csv')
