06df4d6806bfcb5a9d1cafe63e189c03472b9e8e
Adriane Hanson <ahansonuga@gmail.com>
2020-10-13 10:49:07 -0400

    Updated script description, reports.py

--- scripts/reports.py
+++ scripts/reports.py
@@ -1,5 +1,9 @@
-"""Calculate various subtotal reports of the collection, aip, and file counts for format types, format standard
-names, and groups. The reports are saved as csv files.
+"""Calculates subtotals of collection, AIP, and file counts for different categories: format types, format standardized
+names, groups, and combinations of those categories. The results are saved to an Excel file.
+
+This script uses the merged ARCHive formats CSV, which is created with the csv_merge.py script. This CSV is organized
+by group and then by format name. Relevant columns for this analysis are Group, Collection_Count, AIP_Count,
+File_Count, Format_Type, Format_Standardized_Name, and Format_Name.
 
 Usage: python /path/reports.py /path/formats_csv"""
 
