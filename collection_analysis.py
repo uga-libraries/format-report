c58e5bd8ea9952f316b1bb47d447f4d0e497ed22
Adriane Hanson <ahansonuga@gmail.com>
2020-10-26 16:33:13 -0400

    Create collection_analysis.py
    Realized analysis with pandas has potentially a lot of duplication. Calculating instead the unique number of collections per format type using a dictionary.

---
+++ collection_analysis.py
@@ -0,0 +1,79 @@
+# Subtotal for collections may be counting the same collection many times.
+# What do the numbers look like for format types if calculated by collection?
+
+"""
+Duplication makes a big difference. Counts below are before and after deduplication with 10/26 data:
+
+application: 266, 53
+archive: 65, 27
+audio: 154, 69
+database: 65, 14
+design: 70, 17
+executable: 155, 21
+geographic_data: 7, 1
+image: 1174, 538
+message: 50, 14
+model: 2, 2
+multipart: 2, 2
+presentation: 48, 24
+spreadsheet: 124, 27
+structured_text: 131, 26
+text: 557, 63
+video: 476, 160
+web_archive: 18, 17
+
+"""
+
+import csv
+
+formats_report = "C:/users/amhan/Documents/GitHub/format-report/testing/2020-10-26_prod/archive_formats_2020-10.csv"
+
+type_coll_id = {}
+type_coll_count = {}
+
+# Gets the data from the merged ARCHive formats report.
+with open(formats_report, 'r') as formats:
+    formats_read = csv.reader(formats)
+
+    # Skips the header row.
+    next(formats_read)
+
+    # Gets the data from each row in the report, which has information about a single format for that group.
+    # row[0] is group.
+    # row[4] is format type.
+    # row[11] is collection ids (a comma separated string).
+    for row in formats_read:
+
+        # Format the collections as a list.
+        collection_list = set(row[11].split(', '))
+
+        # For Russell, remove the dash from collection identifiers, since there can be two id formats for the same
+        # collection, rbrl-### and rbrl###. If both variations are present, just want to count it once.
+        if row[0] == 'russell':
+            collection_list = [collection.replace('-', '') for collection in collection_list]
+
+        # Change to a set, which removes duplicates from Russell reformatting.
+        collection_list = list(set(collection_list))
+
+        # Adds each collection to a dictionary with format type as the key and a list of collection ids as the value.
+        for collection in collection_list:
+            try:
+                type_coll_id[row[4]].append(collection)
+            except KeyError:
+                type_coll_id[row[4]] = [collection]
+
+    # Add pre-deduplication count to count dictionary.
+    for key, value in type_coll_id.items():
+        type_coll_count[key] = [len(value)]
+
+    # Changes each list in the dictionary to a set to remove duplicates.
+    for key, value in type_coll_id.items():
+        type_coll_id[key] = set(value)
+
+    # Add the post-deduplication count to count dictionary
+    for key, value in type_coll_id.items():
+        type_coll_count[key].append(len(value))
+
+    # Prints the results:
+    for key, value in type_coll_count.items():
+        print(key, value)
