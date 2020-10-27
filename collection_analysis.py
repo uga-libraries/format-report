2379a6c7a4ac02e9ff85e9ea582cbb3d09671065
Adriane Hanson <ahansonuga@gmail.com>
2020-10-27 12:07:26 -0400

    Remove pre-duplication calculation, collection_analysis.py
    Now that have established the difference is significant, only need the unique count of collections per type in the dictionary.

--- collection_analysis.py
+++ collection_analysis.py
@@ -1,28 +1,6 @@
-# Subtotal for collections counts the same collection many times.
-# What do the numbers look like for format types if calculated by collection?
-
-"""
-Duplication makes a big difference. Counts below are before and after deduplication with 10/26 data:
-
-video [476, 160]
-image [1174, 538]
-audio [154, 69]
-application [266, 53]
-structured_text [131, 26]
-text [557, 63]
-spreadsheet [124, 27]
-archive [65, 27]
-presentation [48, 24]
-executable [155, 21]
-web_archive [18, 17]
-database [65, 14]
-message [50, 14]
-design [70, 17]
-model [2, 2]
-multipart [2, 2]
-geographic_data [7, 1]
-
-"""
+# Calculate the number of unique collections and AIPs per format type and format standardized name. Because of how
+# the data is normalized, the same collection or AIP is counted multiple times if the counts in the archive formats
+# csv are used, resulting in very inflated numbers. Numbers can be 2x-10x bigger.
 
 import csv
 
@@ -52,9 +30,6 @@ with open(formats_report, 'r') as formats:
         if row[0] == 'russell':
             collection_list = [collection.replace('-', '') for collection in collection_list]
 
-        # Change to a set, which removes duplicates from Russell reformatting.
-        collection_list = list(set(collection_list))
-
         # Adds each collection to a dictionary with format type as the key and a list of collection ids as the value.
         for collection in collection_list:
             try:
@@ -62,18 +37,10 @@ with open(formats_report, 'r') as formats:
             except KeyError:
                 type_coll_id[row[4]] = [collection]
 
-    # Add pre-deduplication count to count dictionary.
+    # Add the number of unique collections to count dictionary. Make a set first to remove duplicates.
     for key, value in type_coll_id.items():
-        type_coll_count[key] = [len(value)]
+        type_coll_count[key] = len(set(value))
 
-    # Changes each list in the dictionary to a set to remove duplicates.
-    for key, value in type_coll_id.items():
-        type_coll_id[key] = set(value)
-
-    # Add the post-deduplication count to count dictionary
-    for key, value in type_coll_id.items():
-        type_coll_count[key].append(len(value))
-
-    # Prints the results:
+    # Prints the results.
     for key, value in type_coll_count.items():
         print(key, value)
