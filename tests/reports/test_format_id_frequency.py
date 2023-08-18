110cc07d8fb0b55af89bbbd9fe37f912b025604f
Adriane Hanson <ahansonuga@gmail.com>
2023-08-18 08:45:11 -0400

    Make test_format_id_frequency

---
+++ tests/reports/test_format_id_frequency.py
@@ -0,0 +1,35 @@
+"""
+Test for the function format_id_frequency(),
+which calculates the number and percentage of files and size for each format id.
+"""
+
+import os
+import pandas as pd
+import unittest
+from reports import format_id_frequency
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_format_id_frequency(self):
+        """
+        Test for running the function. There are no variations to test.
+        """
+        # Calculates the values of variables used for function input.
+        totals_dict = {"Collections": 7, "AIPs": 14, "Files": 2290, "Size": 3326.99}
+        df_formats = pd.read_csv(os.path.join("format_id_frequency", "archive_formats_2023-08.csv"))
+
+        # Runs the function being tested and converts the output into a list for easier comparison.
+        format_ids = format_id_frequency(totals_dict, df_formats)
+        result = [format_ids.columns.tolist()] + format_ids.reset_index().values.tolist()
+
+        # Tests if the function output has the expected values.
+        expected = [["File_IDs", "File_IDs Percentage", "Size (GB)", "Size (GB) Percentage"],
+                    ["Tagged Image File Format|6|fmt/353", 735, 32.10, 1540.2, 46.29],
+                    ["Matroska|NO VALUE|NO VALUE", 645, 28.17, 1487.702, 44.72],
+                    ["JPEG EXIF|2.1|x-fmt/390", 195, 8.52, 130.0, 3.91]]
+        self.assertEqual(result, expected, "Problem with test for format id frequency")
+
+
+if __name__ == '__main__':
+    unittest.main()
