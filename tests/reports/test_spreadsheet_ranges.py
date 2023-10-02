a2025da94b2bd055ad53b5ec273ee003ccf66d77
Adriane Hanson <ahansonuga@gmail.com>
2023-10-02 17:50:25 -0400

    New input for test_spreadsheet_ranges

--- tests/reports/test_spreadsheet_ranges.py
+++ tests/reports/test_spreadsheet_ranges.py
@@ -1,6 +1,6 @@
 """
 Test for the function spreadsheet_ranges(),
-which makes a spreadsheet with the number of instances of format types and format standardized names
+which makes a spreadsheet with the number of instances of format standardized names and ids
 within predetermined ranges of file id counts or size.
 """
 import os
@@ -13,7 +13,7 @@ class MyTestCase(unittest.TestCase):
 
     def tearDown(self):
         """Deletes the Excel spreadsheet produced by the function, if it is made by the test."""
-        file_path = os.path.join("correct_input", "ARCHive-Formats-Analysis_Ranges.xlsx")
+        file_path = os.path.join("spreadsheet_ranges", "ARCHive-Formats-Analysis_Ranges.xlsx")
         if os.path.exists(file_path):
             os.remove(file_path)
 
@@ -23,15 +23,15 @@ class MyTestCase(unittest.TestCase):
         There is no error handling or variations of input to test.
         """
         # Makes the dataframe used for function input.
-        df_formats_by_group = pd.read_csv(os.path.join("correct_input", "archive_formats_by_group_2023-08.csv"))
+        df_formats_by_group = pd.read_csv(os.path.join("spreadsheet_ranges", "archive_formats_by_group_2023-08.csv"))
 
         # Runs the function being tested.
-        spreadsheet_ranges(df_formats_by_group, "correct_input")
+        spreadsheet_ranges(df_formats_by_group, "spreadsheet_ranges")
 
         # Reads the entire Excel file into pandas, and then each sheet into a separate dataframe.
         # Reading all the sheets at once so the Excel file can be closed,
         # allowing it to be deleted even if there are errors during the tests.
-        result = pd.ExcelFile(os.path.join("correct_input", "ARCHive-Formats-Analysis_Ranges.xlsx"))
+        result = pd.ExcelFile(os.path.join("spreadsheet_ranges", "ARCHive-Formats-Analysis_Ranges.xlsx"))
         df_1 = pd.read_excel(result, "Format Name Ranges")
         df_2 = pd.read_excel(result, "Format Name Sizes")
         df_3 = pd.read_excel(result, "Format ID Ranges")
@@ -41,27 +41,27 @@ class MyTestCase(unittest.TestCase):
         # Tests if the Format Name Ranges sheet has the expected values.
         result_1 = [df_1.columns.tolist()] + df_1.values.tolist()
         expected_1 = [["File_ID Count Range", "Number of Formats (Format Standardized Name)"],
-                      ["1-9", 0], ["10-99", 1], ["100-999", 1], ["1000-9999", 3], ["10000-99999", 1], ["100000+", 1]]
+                      ["1-9", 2], ["10-99", 0], ["100-999", 3], ["1000-9999", 2], ["10000-99999", 1], ["100000+", 3]]
         self.assertEqual(result_1, expected_1, "Problem with test for Format Name Ranges")
 
         # Tests if the Format Name Sizes sheet has the expected values.
         result_2 = [df_2.columns.tolist()] + df_2.values.tolist()
         expected_2 = [["Size Range", "Total Size (Format Standardized Name)"],
-                      ["0-9 GB", 1], ["10-99 GB", 1], ["100-499 GB", 1], ["500-999 GB", 1], ["1-9 TB", 2],
-                      ["10-49 TB", 0], ["50+ TB", 1]]
+                      ["0-9 GB", 5], ["10-99 GB", 1], ["100-499 GB", 2], ["500-999 GB", 1], ["1-9 TB", 1],
+                      ["10-49 TB", 1], ["50+ TB", 0]]
         self.assertEqual(result_2, expected_2, "Problem with test for Format Name Sizes")
 
         # Tests if the Format ID Ranges sheet has the expected values.
         result_3 = [df_3.columns.tolist()] + df_3.values.tolist()
         expected_3 = [["File_ID Count Range", "Number of Formats (Format Identification)"],
-                      ["1-9", 0], ["10-99", 1], ["100-999", 3], ["1000-9999", 6], ["10000-99999", 1], ["100000+", 1]]
+                      ["1-9", 3], ["10-99", 2], ["100-999", 2], ["1000-9999", 2], ["10000-99999", 1], ["100000+", 3]]
         self.assertEqual(result_3, expected_3, "Problem with test for correct input, Format ID Ranges")
 
         # Tests if the Format ID Sizes sheet has the expected values.
         result_4 = [df_4.columns.tolist()] + df_4.values.tolist()
         expected_4 = [["Size Range", "Total Size (Format Identification)"],
-                      ["0-9 GB", 5], ["10-99 GB", 2], ["100-499 GB", 1], ["500-999 GB", 1], ["1-9 TB", 2],
-                      ["10-49 TB", 0], ["50+ TB", 1]]
+                      ["0-9 GB", 7], ["10-99 GB", 1], ["100-499 GB", 2], ["500-999 GB", 1], ["1-9 TB", 1],
+                      ["10-49 TB", 1], ["50+ TB", 0]]
         self.assertEqual(result_4, expected_4, "Problem with test for Format ID Sizes")
 
 
