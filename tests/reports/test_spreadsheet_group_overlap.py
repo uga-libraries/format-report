d232e1105e0b0d24a40b0dc2ac55f47a36a21e08
Adriane Hanson <ahansonuga@gmail.com>
2023-10-02 17:20:38 -0400

    New input for test_spreadsheet_group_overlap
    Had used input also used for testing the script. Wanted more variation.

--- tests/reports/test_spreadsheet_group_overlap.py
+++ tests/reports/test_spreadsheet_group_overlap.py
@@ -23,15 +23,16 @@ class MyTestCase(unittest.TestCase):
         There is no error handling or variations of input to test.
         """
         # Makes the dataframe used for function input.
-        df_formats_by_group = pd.read_csv(os.path.join("correct_input", "archive_formats_by_group_2023-08.csv"))
+        df_formats_by_group = pd.read_csv(os.path.join("spreadsheet_group_overlap",
+                                                       "archive_formats_by_group_2023-08.csv"))
 
         # Runs the function being tested.
-        spreadsheet_group_overlap(df_formats_by_group, "correct_input")
+        spreadsheet_group_overlap(df_formats_by_group, "spreadsheet_group_overlap")
 
         # Reads the entire Excel file into pandas, and then each sheet into a separate dataframe.
         # Reading all the sheets at once so the Excel file can be closed,
         # allowing it to be deleted even if there are errors during the tests.
-        result = pd.ExcelFile(os.path.join("correct_input", "ARCHive-Formats-Analysis_Group-Overlap.xlsx"))
+        result = pd.ExcelFile(os.path.join("spreadsheet_group_overlap", "ARCHive-Formats-Analysis_Group-Overlap.xlsx"))
         df_1 = pd.read_excel(result, "Groups per Type")
         df_2 = pd.read_excel(result, "Groups per Name")
         df_3 = pd.read_excel(result, "Groups per Format ID")
@@ -40,39 +41,31 @@ class MyTestCase(unittest.TestCase):
         # Tests if the Groups per Type sheet has the expected values.
         result_1 = [df_1.columns.tolist()] + df_1.values.tolist()
         expected_1 = [["Format Type", "Groups", "Group List"],
-                      ["audio", 2, "bmac, dlg"],
-                      ["image", 2, "dlg, hargrett"],
-                      ["video", 2, "bmac, dlg"],
-                      ["web_archive", 1, "hargrett"]]
+                      ["image", 4, "bmac, dlg, dlg-hargrett, dlg-magil"],
+                      ["structured_text", 3, "dlg, hargrett, russell"],
+                      ["design", 1, "russell"],
+                      ["text", 1, "russell"]]
         self.assertEqual(result_1, expected_1, "Problem with test for Groups per Type")
 
         # Tests if the Groups per Name sheet has the expected values.
         result_2 = [df_2.columns.tolist()] + df_2.values.tolist()
         expected_2 = [["Format Standardized Name", "Groups", "Group List"],
-                      ["JPEG", 2, "hargrett, dlg"],
-                      ["Matroska", 2, "bmac, dlg"],
-                      ["TIFF", 2, "dlg, hargrett"],
-                      ["JP2", 1, "dlg"],
-                      ["WARC", 1, "hargrett"],
-                      ["WAVE", 1, "bmac"],
-                      ["Waveform Audio", 1, "dlg"]]
+                      ["HTML", 3, "dlg, hargrett, russell"],
+                      ["TIFF", 3, "dlg, dlg-hargrett, dlg-magil"],
+                      ["Adobe Illustrator", 1, "russell"],
+                      ["Digital Moving Picture Exchange Bitmap", 1, "bmac"],
+                      ["WordPerfect", 1, "russell"]]
         self.assertEqual(result_2, expected_2, "Problem with test for Groups per Name")
 
         # Tests if the Groups per Format ID sheet has the expected values.
         result_3 = [df_3.columns.tolist()] + df_3.values.tolist()
         expected_3 = [["Format Identification", "Groups", "Group List"],
-                      ["JPEG File Interchange Format|1.02|fmt/44", 2, "dlg, hargrett"],
-                      ["JPEG File Interchange Format|1|fmt/42", 2, "dlg, hargrett"],
-                      ["Matroska|NO VALUE|NO VALUE", 2, "bmac, dlg"],
-                      ["Tagged Image File Format|NO VALUE|NO VALUE", 2, "dlg, hargrett"],
-                      ["JPEG 2000 JP2|NO VALUE|x-fmt/392", 1, "dlg"],
-                      ["JPEG EXIF|2.1|x-fmt/390", 1, "hargrett"],
-                      ["JPEG File Interchange Format|1.01|fmt/43", 1, "hargrett"],
-                      ["Tagged Image File Format|5|NO VALUE", 1, "dlg"],
-                      ["Tagged Image File Format|6|fmt/353", 1, "dlg"],
-                      ["WARC|NO VALUE|fmt/289", 1, "hargrett"],
-                      ["Waveform Audio|NO VALUE|NO VALUE", 1, "dlg"],
-                      ["Wave|NO VALUE|NO VALUE", 1, "bmac"]]
+                      ["Tagged Image File Format|5.0|fmt/353", 3, "dlg, dlg-hargrett, dlg-magil"],
+                      ["Hypertext Markup Language|NO VALUE|NO VALUE", 2, "dlg, russell"],
+                      ["Adobe Illustrator|5.0 / 5.5|fmt/420", 1, "russell"],
+                      ["DPX|NO VALUE|NO VALUE", 1, "bmac"],
+                      ["Hypertext Markup Language|NO VALUE|fmt/96", 1, "hargrett"],
+                      ["Wordperfect Secondary File|5.0|x-fmt/42", 1, "russell"]]
         self.assertEqual(result_3, expected_3, "Problem with test for Groups per Format ID")
 
 
