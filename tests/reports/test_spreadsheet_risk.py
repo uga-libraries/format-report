dd6d68892ad100cd0fedd853ff01521941779007
Adriane Hanson <ahansonuga@gmail.com>
2023-10-23 11:45:52 -0400

    Rename variables
    Using subject description (archive_risk, archive_result, etc.) rather than numbering (df_1, result_1, etc.) to be more descriptive and more flexible in the future. This way, tests can be added or removed without redoing the numbering sequence.

--- tests/reports/test_spreadsheet_risk.py
+++ tests/reports/test_spreadsheet_risk.py
@@ -16,10 +16,11 @@ class MyTestCase(unittest.TestCase):
         if os.path.exists(file_path):
             os.remove(file_path)
 
-    def test_development(self):
+    def test_spreadsheet_risk(self):
         """
-        Using this for building up complexity of this function.
-        Variations needed: cross-group duplicates and unique for every category, all levels and not all levels present.
+        Test for the function working correctly.
+        There is no error handling or variations of input to test.
+        Input variations are handled in the function groupby_risk(), which is called by this function.
         """
         # Makes the dataframe used for function input.
         # For now, only including one field in df_group beyond what need for this analysis and data is fake.
@@ -32,55 +33,55 @@ class MyTestCase(unittest.TestCase):
         # Reading all the sheets at once so the Excel file can be closed,
         # allowing it to be deleted even if there are errors during the tests.
         result = pd.ExcelFile(os.path.join("spreadsheet_risk", "ARCHive-Formats-Analysis_Risk.xlsx"))
-        df_1 = pd.read_excel(result, "ARCHive Risk Overview")
-        df_2 = pd.read_excel(result, "Department Risk Overview")
-        df_3 = pd.read_excel(result, "Format Type Risk")
-        df_5 = pd.read_excel(result, "NARA Match Types")
+        archive_risk = pd.read_excel(result, "ARCHive Risk Overview")
+        dept_risk = pd.read_excel(result, "Department Risk Overview")
+        type_risk = pd.read_excel(result, "Format Type Risk")
+        match = pd.read_excel(result, "NARA Match Types")
         result.close()
 
         # Tests if the ARCHive Risk Overview sheet has the expected values.
-        result_1 = [df_1.columns.tolist()] + df_1.values.tolist()
-        expected_1 = [["NARA_Risk Level", "File_IDs", "Size (GB)", "Format Identifications"],
-                      ["Low Risk", 7, 70, 2],
-                      ["Moderate Risk", 3, 30, 1],
-                      ["High Risk", 4, 40, 1],
-                      ["No Match", 5, 50, 1]]
-        self.assertEqual(result_1, expected_1, "Problem with the test for ARCHive Risk Overview")
+        archive_result = [archive_risk.columns.tolist()] + archive_risk.values.tolist()
+        archive_expected = [["NARA_Risk Level", "File_IDs", "Size (GB)", "Format Identifications"],
+                            ["Low Risk", 7, 70, 2],
+                            ["Moderate Risk", 3, 30, 1],
+                            ["High Risk", 4, 40, 1],
+                            ["No Match", 5, 50, 1]]
+        self.assertEqual(archive_result, archive_expected, "Problem with the test for ARCHive Risk Overview")
 
         # Tests if the Department Risk Overview sheet has the expected values.
-        result_2 = [df_2.columns.tolist()] + df_2.values.tolist()
-        expected_2 = [["Group", "NARA_Risk Level", "File_IDs", "Size (GB)", "Format Identifications"],
-                      ["A", "Low Risk", 3, 30, 1],
-                      ["A", "Moderate Risk", 3, 30, 1],
-                      ["A", "High Risk", 0, 0, 0],
-                      ["A", "No Match", 5, 50, 1],
-                      ["B", "Low Risk", 4, 40, 2],
-                      ["B", "Moderate Risk", 0, 0, 0],
-                      ["B", "High Risk", 4, 40, 1],
-                      ["B", "No Match", 0, 0, 0]]
-        self.assertEqual(result_2, expected_2, "Problem with the test for Department Risk Overview")
+        dept_result = [dept_risk.columns.tolist()] + dept_risk.values.tolist()
+        dept_expected = [["Group", "NARA_Risk Level", "File_IDs", "Size (GB)", "Format Identifications"],
+                         ["A", "Low Risk", 3, 30, 1],
+                         ["A", "Moderate Risk", 3, 30, 1],
+                         ["A", "High Risk", 0, 0, 0],
+                         ["A", "No Match", 5, 50, 1],
+                         ["B", "Low Risk", 4, 40, 2],
+                         ["B", "Moderate Risk", 0, 0, 0],
+                         ["B", "High Risk", 4, 40, 1],
+                         ["B", "No Match", 0, 0, 0]]
+        self.assertEqual(dept_result, dept_expected, "Problem with the test for Department Risk Overview")
 
         # Tests if the Format Type Risk sheet has the expected values.
-        result_3 = [df_3.columns.tolist()] + df_3.values.tolist()
-        expected_3 = [["Format Type", "NARA_Risk Level", "File_IDs", "Size (GB)", "Format Identifications"],
-                      ["T1", "Low Risk", 7, 70, 2],
-                      ["T1", "Moderate Risk", 0, 0, 0],
-                      ["T1", "High Risk", 4, 40, 1],
-                      ["T1", "No Match", 0, 0, 0],
-                      ["T2", "Low Risk", 0, 0, 0],
-                      ["T2", "Moderate Risk", 3, 30, 1],
-                      ["T2", "High Risk", 0, 0, 0],
-                      ["T2", "No Match", 5, 50, 1]]
-        self.assertEqual(result_3, expected_3, "Problem with the test for Format Type Risk")
+        type_result = [type_risk.columns.tolist()] + type_risk.values.tolist()
+        type_expected = [["Format Type", "NARA_Risk Level", "File_IDs", "Size (GB)", "Format Identifications"],
+                         ["T1", "Low Risk", 7, 70, 2],
+                         ["T1", "Moderate Risk", 0, 0, 0],
+                         ["T1", "High Risk", 4, 40, 1],
+                         ["T1", "No Match", 0, 0, 0],
+                         ["T2", "Low Risk", 0, 0, 0],
+                         ["T2", "Moderate Risk", 3, 30, 1],
+                         ["T2", "High Risk", 0, 0, 0],
+                         ["T2", "No Match", 5, 50, 1]]
+        self.assertEqual(type_result, type_expected, "Problem with the test for Format Type Risk")
 
         # Tests if the NARA Match Types sheet has the expected values.
-        result_5 = [df_5.columns.tolist()] + df_5.values.tolist()
-        expected_5 = [["NARA_Match_Type", "File_IDs", "Size (GB)", "Format Identifications"],
-                      ["Extension", 7, 70, 2],
-                      ["Name", 2, 20, 1],
-                      ["No NARA Match", 5, 50, 1],
-                      ["PUID", 5, 50, 1]]
-        self.assertEqual(result_5, expected_5, "Problem with the test for NARA Match Types")
+        match_result = [match.columns.tolist()] + match.values.tolist()
+        match_expected = [["NARA_Match_Type", "File_IDs", "Size (GB)", "Format Identifications"],
+                          ["Extension", 7, 70, 2],
+                          ["Name", 2, 20, 1],
+                          ["No NARA Match", 5, 50, 1],
+                          ["PUID", 5, 50, 1]]
+        self.assertEqual(match_result, match_expected, "Problem with the test for NARA Match Types")
 
 
 if __name__ == '__main__':
