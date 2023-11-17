35df2e4d611a502ade723ff6a02eef3445ab86b0
Adriane Hanson <ahansonuga@gmail.com>
2023-11-17 14:33:05 -0500

    check_argument() works with correct input

--- fix_versions.py
+++ fix_versions.py
@@ -6,9 +6,28 @@ The correct version is still part of Format_Identification.
 This script updates the version column with the version from Format_Identification.
 """
 
-# Usage: python path/fix_versions.py path_csv
+# Usage: python path/fix_versions.py csv_path
 #    path_csv is the path to either CSV created b merge_format_reports.py
 
 import os
 import pandas as pd
 import sys
+
+
+def check_argument(argument_list):
+    """
+    Verifies the required argument csv_path is present and valid,
+    and the filename matches expected naming conventions.
+    Returns the path and the error, if any.
+    """
+    # Makes variables with default values to store the results of the function.
+    path = None
+    error = None
+
+    # Verifies that the required argument (csv_path) is present.
+    if len(argument_list) > 1:
+        path = argument_list[1]
+    else:
+        error = "Required argument csv_path is missing"
+
+    return path, error
--- tests/fix_versions/test_check_argument.py
+++ tests/fix_versions/test_check_argument.py
@@ -1,9 +1,31 @@
+"""
+Tests for the function check_argument(),
+which verifies that the required argument is present, valid, and named correctly,
+and returns the path and the error (if any).
+
+For input, tests use a list with argument values. In production, this would be the contents of sys.argv.
+"""
+
+import os
 import unittest
+from fix_versions import check_argument
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_correct(self):
+        """
+        Test for when the required argument is present and correct.
+        """
+        # Runs the function being tested.
+        test_csv = os.path.join("check_argument", "archive_formats_by_group_2023-11.csv")
+        csv_path, error_msg = check_argument(["fix_versions.py", test_csv])
+
+        # Tests that the value of csv_path is correct.
+        self.assertEqual(csv_path, test_csv, "Problem with test for correct, csv_path")
+
+        # Tests that the value of error_msg is correct.
+        self.assertEqual(error_msg, None, "Problem with test for correct, error_msg")
 
 
 if __name__ == '__main__':
