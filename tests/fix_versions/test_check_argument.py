f8e90ca5cffd5ca99feb12b4fed505e5f18ddb67
Adriane Hanson <ahansonuga@gmail.com>
2023-11-17 14:35:53 -0500

    check_argument() works if argument is missing

--- tests/fix_versions/test_check_argument.py
+++ tests/fix_versions/test_check_argument.py
@@ -27,6 +27,20 @@ class MyTestCase(unittest.TestCase):
         # Tests that the value of error_msg is correct.
         self.assertEqual(error_msg, None, "Problem with test for correct, error_msg")
 
+    def test_missing_arg(self):
+        """
+        Test for when the required argument is missing.
+        """
+        # Runs the function being tested.
+        csv_path, error_msg = check_argument(["fix_versions.py"])
+
+        # Tests that the value of csv_path is correct.
+        self.assertEqual(csv_path, None, "Problem with test for missing argument, csv_path")
+
+        # Tests that the value of error_msg is correct.
+        expected_msg = "Required argument csv_path is missing"
+        self.assertEqual(error_msg, expected_msg, "Problem with test for missing argument, error_msg")
+
 
 if __name__ == '__main__':
     unittest.main()
